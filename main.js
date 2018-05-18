c816e54e4a2bddd597332ef2917957108b5019bd
Arthur Thomas <acthomasiv@gmail.com>
2018-05-18 17:51:21 -0400

    Init

---
+++ main.js
@@ -0,0 +1,8 @@
+function resumeFooter() {
+    foot.style.paddingTop = "65vh";
+    foot.backgroundColor = "#337ab7";
+}
+
+function load_rmd() {
+    document.getElementById("content").innerHTML='<object type="text/html" data="finalproject.html" ></object>';
+}
