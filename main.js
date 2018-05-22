9fba6d41b4c300210b35efb69c55ffd1753c244d
Arthur Thomas IV <acthomasiv@gmail.com>
2018-05-22 17:10:47 -0400

    Some updates
    nothing major

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-function resumeFooter() {
-    foot.style.paddingTop = "65vh";
-    foot.backgroundColor = "#337ab7";
+function main() {
+
+    function resumeFooter() {
+        foot.style.paddingTop = "65vh";
+        foot.backgroundColor = "#337ab7";
+    }
+
+  
+
+
+    $(function loadRMD() {
+        $("#rmd").load("finalproject.html");
+    });
+
 }
 
 function load_rmd() {
-    document.getElementById("content").innerHTML='<object type="text/html" data="finalproject.html" ></object>';
+    document.getElementById("content").innerHTML = '<object type="text/html" data="finalproject.html" ></object>';
 }
