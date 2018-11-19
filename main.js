52e38716d83b340f4d8505fd8406519a7279ff62
Arthur Thomas <acthomasiv@gmail.com>
2018-11-18 23:22:17 -0500

    Redesign
    playing around with a more simple and work focused design

--- main.js
+++ main.js
@@ -22,3 +22,36 @@ $('#res').collapse("toggle");
 
 let date = new Date(document.lastModified);
 document.getElementById("modDate").innerHTML = date;
+
+function buttonRead() {
+    $.get("https://raw.githubusercontent.com/cricketthomas/vuefinder/master/README.md", function (data) {
+        var rawtext = data;
+        console.log(rawtext);
+        var converter = new showdown.Converter(),
+            //text = '# hello, markdown!',
+            html = converter.makeHtml(rawtext);
+
+        var vuefinder = document.getElementById('vuefinder').innerHTML = html;
+
+    });
+}
+/*
+var GitHub = require('github-api');
+
+// by default all requests are unauthenticated
+// unauthenticated clients are limited to 60 request per hour
+var noAuth = new GitHub();
+
+// you can authenticate with username and password
+var passwordAuth = new GitHub({
+  username: 'MY_USERNAME',
+  password: 'MY_PASSWORD'
+});
+
+// you can also provide an OAuth token to authenticate the requests
+var oauthAuth = new GitHub({
+  token: 'MY_OAUTH_TOKEN'
+});
+*/
+
+//https://raw.githubusercontent.com/cricketthomas/vuefinder/master/README.md
