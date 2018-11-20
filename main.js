3809301734af5ce3e7e51a5a9a5ebabfd9128b47
Arthur Thomas IV <acthomasiv@gmail.com>
2018-11-20 16:45:27 -0500

    More redesigning

--- main.js
+++ main.js
@@ -22,19 +22,21 @@ function resumeFooter() {
 let date = new Date(document.lastModified);
 document.getElementById("modDate").innerHTML = date;
 
-function buttonRead() {
-    $.get("https://raw.githubusercontent.com/cricketthomas/vuefinder/master/README.md", function (data) {
+function buttonRead(url) {
+
+    //readMe = document.getElementById('readme');
+    //readMe.classList.add('scaled')
+    //insertDiv = document.getElementById("insertDiv");
+    //insertDiv.classList.add("scaled");
+    $.get(url, function (data) {
         var rawtext = data;
         console.log(rawtext);
         var converter = new showdown.Converter(),
             //text = '# hello, markdown!',
             html = converter.makeHtml(rawtext);
-        var vuefinder = document.getElementById('vuefinder').innerHTML = html;
-
-
+        insertDiv.innerHTML = '<h1 class="top">Project Description</h1> <br/>' + html;
+        //readMe.addCl + "scaled"
     });
-
-
 }
 /*
 var GitHub = require('github-api');
