0481755984d4f28861c2eb8cdd257e5fad8a889b
Arthur Thomas <athomas@sandscap.com>
2025-11-12 19:13:28 -0500

    remove unused code

--- main.js
+++ main.js
@@ -1,59 +1,9 @@
-function main() {
-
-    let date = new Date(document.lastModified);
-    //document.getElementById("modDate").innerHTML = date;
-
-} //onload
-
-
-
-
-let date = new Date(document.lastModified);
-//document.getElementById("modDate").innerHTML = date;
-
 function readMe(url) {
-
-    //readMe = document.getElementById('readme');
-    //readMe.classList.add('scaled')
-    //insertDiv = document.getElementById("insertDiv");
-    //insertDiv.classList.add("scaled");
     $.get(url, function (data) {
         var rawtext = data;
         console.log(rawtext);
         var converter = new showdown.Converter(),
-            //text = '# hello, markdown!',
-            html = converter.makeHtml(rawtext);
+        html = converter.makeHtml(rawtext);
         insertDiv.innerHTML = '<h1 class="top">Project Description</h1> <br/>' + html;
-        //readMe.addCl + "scaled"
     });
 }
-/*
-var GitHub = require('github-api');
-
-// by default all requests are unauthenticated
-// unauthenticated clients are limited to 60 request per hour
-var noAuth = new GitHub();
-
-// you can authenticate with username and password
-var passwordAuth = new GitHub({
-  username: 'MY_USERNAME',
-  password: 'MY_PASSWORD'
-});
-
-
-var converter = new showdown.Converter();
-    var md = '[**Showdown**](http://www.showdownjs.com) is *great*\n' +
-        'because:\n\n' +
-        ' - it\'s easy to use\n' +
-        ' - it\'s extensible\n' +
-        ' - works in the server and in the browser';
-    var html = converter.makeHtml(md);
-
-
-// you can also provide an OAuth token to authenticate the requests
-var oauthAuth = new GitHub({
-  token: 'MY_OAUTH_TOKEN'
-});
-*/
-
-//https://raw.githubusercontent.com/cricketthomas/vuefinder/master/README.md
